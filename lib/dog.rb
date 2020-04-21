8904d7c35c37d94607c212bf410a2adc142109d7
RyanLetterio42 <rletterio42@gmail.com>
2020-04-21 17:18:44 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+    def initialize
+        @name = name
+    end
+
+    def initialize
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+end
